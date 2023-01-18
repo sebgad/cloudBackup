199fabe4090b98b003fcc464f934b62f73fbd91a
Sebastian <Sebastian.Gade@gmail.com>
2023-01-18 21:55:50 +0100

    add filebackup and udev ruleset trigger

--- scripts/install_u2f_key.sh
+++ scripts/install_u2f_key.sh
@@ -14,6 +14,9 @@ add-apt-repository ppa:yubico/stable -y && apt-get update
 apt install -y libu2f-udev libpam-u2f
 curl -o /etc/udev/rules.d/70-u2f.rules https://raw.githubusercontent.com/Yubico/libfido2/main/udev/70-u2f.rules
 
+# Reload udev rules
+udevadm control --reload-rules && udevadm trigger
+
 read -p "Please input username who suppose to use yubikey for authentification?" USERNAME
 # as user, please insert yubikey
 mkdir -p /home/$USERNAME/.config/Yubico
@@ -42,21 +45,27 @@ echo "check output file. Should have the format root:Key"
 cat /root/.config/Yubico/u2f_keys_root
 
 # add yubikey authentification for command sudo
+# Make backup in case something went wrong
+mkdir /root/pam_backup
+cp /etc/pam.d/sudo /root/pam_backup
 echo "add yubikey authentification for command sudo. Please check /etc/pam.d/sudo afterwards."
 perl -i -0pe 's/@include common-auth\n/auth    sufficient      pam_u2f.so      authfile=\/root\/.config\/Yubico\/u2f_keys userpresence=0\n/@include common-auth\n/' /etc/pam.d/sudo 
 
 # add yubikey authentification for command su
+cp /etc/pam.d/su /root/pam_backup
 echo "add yubikey authentification for command su. Please check /etc/pam.d/su afterwards."
 perl -i -0pe 's/auth       sufficient pam_rootok.so\n/auth       sufficient pam_rootok.so\nauth    sufficient      pam_u2f.so      authfile=\/root\/.config\/Yubico\/u2f_keys_root userpresence=0\n/' /etc/pam.d/su
 
 # add yubikey authentification for login over TTY
+cp /etc/pam.d/login /root/pam_backup
 echo "add yubikey authentification for command su. Please check /etc/pam.d/login afterwards."
 perl -i -0pe 's/@include common-auth\n/auth    sufficient      pam_u2f.so      authfile=\/root\/.config\/Yubico\/u2f_keys userpresence=0\n@include common-auth\n/' /etc/pam.d/login
+
+echo "backups for /etc/pam.d/sudo /etc/pam.d/su /etc/pam.d/login were copied to /root/pam_backup, in case something went wrong."
 
 # print Out a random passwort of the length 64
 echo "Please use a strong root password. For example this one:"
 < /dev/urandom tr -cd "[:print:]" | head -c 64; echo
 
-
-echo "please reboot"
+echo "please check login. Changes should already be effective."
 
